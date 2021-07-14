89e83b1151aa8fe815b53ca12e6595ad5ade8c7b
Wenfei Zhu <wenfei.zhu@king.com>
2021-07-14 10:32:11 +0200

    add memory to recursion

--- 111.Climbing_stairs.cpp
+++ 111.Climbing_stairs.cpp
@@ -28,29 +28,6 @@ public:
 // class Solution {
 // public:
 //     int climbStairs(int n) {
-//         int memo[n + 1];
-//         return climb_Stairs(0, n, memo);
-//     }
-
-//     int climb_Stairs(int i, int n, int memo[]) {
-//         if (i > n) {
-//             return 0;
-//         }
-//         if (i == n) {
-//             return 1;
-//         }
-//         if (memo[i] > 0) {
-//             return memo[i];
-//         }
-//         memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
-//         return memo[i];
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     int climbStairs(int n) {
 //         if (n <= 2) {
 //             return n;
 //         }
